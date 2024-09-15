938cae2e5901bd5af7094576e47bb9aea1adb648
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-15 18:25:21 -0300

    positivo,negativo ou zero

---
+++ atv13.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+numero= int(input("Digite seu numero:"))
+if numero >0:
+    print("Seu numero e positivo")
+elif numero<0:
+    print("Negativo")
+else:
+    print("Zero")
